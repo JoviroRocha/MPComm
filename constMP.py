f890a162f6262febfd38d47eb6cdce3e62a1b2ae
Joviro Rocha <joviro_neto@hotmail.com>
2023-02-28 18:42:40 -0300

    A

--- constMP.py
+++ constMP.py
@@ -1,7 +1,7 @@
 #To Do: Replace all this with a naming service
-PEERS = ['35.232.71.36','35.202.69.50','35.225.116.27','35.223.245.85']
+PEERS = ['10.128.0.2','10.128.0.3','10.128.0.4','10.128.0.5']
 PORT = 4567
 N = 4
 N_MSGS = 10
-SERVER_ADDR = '34.30.85.249'
+SERVER_ADDR = '10.128.0.6'
 SERVER_PORT = 4568
